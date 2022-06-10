a7c5427b99eb48897c47f7f8f2c5ca3d49ab7172
Denis Korobenko <den.korobenko@ukr.net>
2022-06-10 14:01:02 +0300

    first_commit

---
+++ client/src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Danaykanych Vlad", username: "c0dy", img: img1 },
+  { name: "Miha_Nad", username: "Kaban", img: img2 },
+  { name: "Anton Nemerovich", username: "Math_king", img: img3 },
+  { name: "Ves_Oleg", username: "Kapichon", img: img4 },
+];
---
+++ client/src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Danaykanych Vlad',
+        desc: "Slava Polky Azov!",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Miha_Nad',
+        desc: "Glory to Ukraine!",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Anton Nemerovich",
+        desc: "I love Ukraine!",
+        likes: 800,
+        liked: false
+    }
+]
